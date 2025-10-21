0e92b43801ef5cd12f75c379d9ac689a7593fcd1
SCnudde <125920286+SCnudde@users.noreply.github.com>
2025-10-21 10:50:01 +0300

    Add files via upload

---
+++ Output_figures.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Geometry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            foreach (var item in File.ReadAllLines("Geometry.txt"))
+            {
+                Console.WriteLine(item);
+            }           
+
+        }
+    }
+}
